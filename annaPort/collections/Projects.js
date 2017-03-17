399a5028cb2a347531d70a595cf5d6e066371ec5
Andy <abeaudoin2013@gmail.com>
2017-03-17 09:31:05 -0400

    fixed image deletion

--- annaPort/collections/Projects.js
+++ annaPort/collections/Projects.js
@@ -1,6 +1,27 @@
 Images = new FilesCollection({
   collectionName: 'Images',
-  allowClientCode: true // Required to let you remove uploaded file
+  allowClientCode: true, // Required to let you remove uploaded file
+  onBeforeRemove: function (cursor) {
+    if (this.userId) {
+      var user = this.user();
+      if (Roles.userIsInRole(user, 'admin')) {
+        // Allow removal only if
+        // current user is signed-in
+        // and has role is `admin`
+
+        // FIND PROJECT COLLECTION BY IMAGE ID
+        // UPDATE PICTURES ARRAY IN PROJECT COLLECTION
+        // TO PULL DELETED PICTURE
+        var imgId = cursor._selector;
+        var project = Projects.findOne({pictures: {$in: [imgId]}});
+        var p_id = project._id;
+        Projects.update({"_id": p_id}, {"$pull": {"pictures": imgId}});
+        return true;
+        
+      }
+    }
+    return false;
+  }
 });
 
 Projects = new Mongo.Collection('projects');
